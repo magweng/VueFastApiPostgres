97bdf960dd40ccf5481e8bc226d32f27101b3d96
magweng <magwengg@gmail.com>
2021-02-11 10:18:01 +0100

    init project

---
+++ project/routes.py
@@ -0,0 +1,11 @@
+import fastapi
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
+
+templates = Jinja2Templates(directory="templates")
+
+router = fastapi.APIRouter()
+
+@router.get('/index/{param}', include_in_schema=False)
+def index(request: Request, param: str):
+    return templates.TemplateResponse("index.html", {"request": request, "id": param})
